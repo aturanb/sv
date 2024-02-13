ab2a5fd2dbea06a467d8a177fc8993a6f7603e8a
Turan <ahmet.turan.abi@gmail.com>
2024-02-13 01:56:54 -0800

    Server side sign up implemented

--- server/functions/index.js
+++ server/functions/index.js
@@ -7,14 +7,38 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest, onCall} = require("firebase-functions/v2/https");
+const {onRequest, onCall, HttpsError} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const {initializeApp} = require("firebase-admin/app");
+const admin = require("firebase-admin");
+
 initializeApp();
 
+exports.signUp = onCall(async (request)=> {
+    try {
+        
+        const userData = request.data.personInfo;
+        
+        console.log(userData)
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+        const userRecord = await admin.auth().createUser({
+            email: userData.email,
+            emailVerified: userData.emailVerified || false,
+            phoneNumber: userData.phoneNumber,
+            password: userData.password,
+            displayName: userData.displayName,
+            photoURL: userData.photoURL,
+            disabled: userData.disabled || false
+        });
+        
+        logger.info("Successfully created new user:", userRecord.uid);
+        return { result: `Successfully created new user: ${userRecord.uid}.` };
+    } catch (error) {
+        logger.error("Failed to create an account:", error);
+        throw new HttpsError("unknown", "Failed to create an account.", error);
+    }
+});
+
 
 exports.helloWorld = onCall((request) => {
     const text = request.data.text;
